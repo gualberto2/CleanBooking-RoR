c18623259121a0e01129bd7b2c4c694b7994cc8e
Elijah Wilson <elijahwilsonnn@gmail.com>
2025-05-15 18:03:45 -0700

    stuff

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "tw-animate-css" # @1.2.7
+pin "tw-animate-css" # @1.2.9
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
