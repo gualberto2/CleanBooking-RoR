8a7463c497de7f78f91973cf0c1812830435aedd
Elijah Wilson <elijahwilsonnn@gmail.com>
2025-05-15 18:03:07 -0700

    added some stuff for auth0

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
   root "static_pages#landing_page"
+
   get "dashboard", to: "static_pages#dashboard"
-  devise_for :users
+
+  get "/auth/auth0/callback" => "auth0#callback"
+  get "/auth/failure" => "auth0#failure"
+  get "/auth/logout" => "auth0#logout"
 end
